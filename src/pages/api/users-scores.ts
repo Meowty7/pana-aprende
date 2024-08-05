7a471a56e87390ff3466a416d7c28b9f4a8665b1
Meowty7 <beltazarw@gmail.com>
2024-08-04 21:10:16 -0500

    test

--- src/pages/api/users-scores.ts
+++ src/pages/api/users-scores.ts
@@ -2,25 +2,19 @@ import { db } from "../../lib/db";
 import type { APIContext } from "astro";
 
 export async function GET(context: APIContext): Promise<Response> {
-  try {
+
     const scores = db.prepare(`
-        SELECT u.username, SUM(s.score) AS total_score
-        FROM scores s
-        JOIN user u ON s.user_id = u.id
-        GROUP BY u.username
-        ORDER BY total_score DESC;`
-    ).all();
-    
-    return new Response(JSON.stringify(scores), {
+                    SELECT u.username, SUM(s.score) AS total_score
+                    FROM scores s
+                    JOIN user u ON s.user_id = u.id
+                    GROUP BY u.username
+                    ORDER BY total_score DESC;`
+                    ).all();
+
+     return new Response(JSON.stringify(scores), {
       status: 200,
       headers: {
         'Content-Type': 'application/json',
       },
     });
-  } catch (error) {
-    console.error('Error fetching scores:', error);
-    return new Response('Internal Server Error', {
-      status: 500,
-    });
-  }
 }
